5abb863e137e5289beadea0b6c9a88e5cc2211da
CoolDeath <dobre4ko@gmail.com>
2021-05-18 02:32:33 +0300

    отсортировано

---
+++ Synthesizer/Models/HighPassFilterSampleProvider.cs
@@ -0,0 +1,31 @@
+﻿
+using NAudio.Wave;
+
+using NAudio.Dsp;
+
+namespace Synthesizer
+{
+    class HighPassFilterSampleProvider : ISampleProvider
+    {
+        private readonly ISampleProvider _source;
+        private readonly BiQuadFilter _filter;
+        public WaveFormat WaveFormat => _source.WaveFormat;
+
+        public HighPassFilterSampleProvider(ISampleProvider source, int cutOffFrequency = 50, float q = 0.7f)
+        {
+            _source = source;
+            _filter = BiQuadFilter.HighPassFilter(source.WaveFormat.SampleRate, cutOffFrequency, q);
+        }
+        public int Read(float[] buffer, int offset, int count)
+        {
+
+            var samples = _source.Read(buffer, offset, count);
+            for (int n = 0; n < samples; n++)
+            {
+                buffer[offset + n] *= _filter.Transform(buffer[offset + n]);
+            }
+            return samples;
+
+        }
+    }
+}
---
+++ Synthesizer/Views/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using NAudio.Wave.SampleProviders;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Synthesizer
+{
+    public partial class MainWindow : Window
+    {
+        private readonly MainWindowViewModel _viewModel;
+
+        public MainWindow(MainWindowViewModel _viewModel)
+        {
+            
+            InitializeComponent();
+             this._viewModel = _viewModel;
+             DataContext = _viewModel;
+             Closing += _viewModel.OnWindowClosing;
+            
+        }
+
+       
+        
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            base.OnKeyDown(e);
+            _viewModel.KeyDown(e);
+        }
+
+        protected override void OnKeyUp(KeyEventArgs e)
+        {
+            base.OnKeyUp(e);
+            _viewModel.KeyUp(e);
+        }
+
+        
+       
+
+       
+        
+       
+
+    
+    }
+}
